c1609f0836bc1db6303a6fc776a1aa35c54703c9
sulta <sulta@DESKTOP-E785JP2>
2023-10-03 21:40:18 +0300

    Добавьте файлы проекта.

---
+++ dz8/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите максимально целое число диапозона");
+            int max = int.Parse(Console.ReadLine());
+            Random rand = new Random();
+            int iiNumber = rand.Next(1,max);
+            int count = 0;
+            int usernumber; 
+            while (true)
+            {
+                Console.WriteLine("\nВведите число");
+                count++;
+                usernumber = int.Parse(Console.ReadLine());
+                if (usernumber < iiNumber)
+                {
+                    Console.WriteLine("Ваше число меньше загаданного");
+                }
+                else if (usernumber > iiNumber)
+                {
+                    Console.WriteLine("Ваше число больше загаданного");
+                }
+                else if (usernumber ==  iiNumber)
+                {
+                    Console.WriteLine("Вы угадали");
+                    Console.ReadKey();
+                    break;
+                }
+            }
+        }
+    }
+}
